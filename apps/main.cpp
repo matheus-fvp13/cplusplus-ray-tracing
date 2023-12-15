229e729af6a4c333ae4b5e37e2b8017e3441826b
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2023-12-15 15:24:05 -0300

    vec3 class

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
 #include "image_manager.h"
+#include "vec3.h"
 
 int main() {
 
-    ImageManager imageManager = ImageManager();
-    imageManager.saveSquareImage("./assets/images/square.png", 640, 480, "green", "red");
-    imageManager.saveDegradeImage("./assets/images/gradiente.png", 640, 480, "blue", "orange");
-    imageManager.saveCircleImage("./assets/images/circle.png", 640, 480, "blue", "orange");
+    vec3 v(1.0, 2.0, 4.0);
+    std::cout << v.x() << std::endl;
     return 0;
 }
