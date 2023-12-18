50e1803f099b1d9a89b19bc8e1a45fad601b3d70
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2023-12-18 11:30:41 -0300

    positionable camera

--- apps/main.cpp
+++ apps/main.cpp
@@ -15,27 +15,38 @@
 
 int main() {
 
-    ObjLoader objLoader;
-    std::vector<triangle> triangles = objLoader.readObj("./assets/objects/read/seahorse.obj", vec3(0, 0, 0));
-
-    // World
-
+    //ObjLoader objLoader;
+    //std::vector<triangle> triangles = objLoader.readObj("./assets/objects/read/seahorse.obj", vec3(0, 0, 0));
     hittable_list world;
-    for(auto& t: triangles) {
-        world.add(make_shared<triangle>(t));
-    }
+
+
+    auto R = cos(pi/4);
+
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5));
+    //for(auto& t: triangles) {
+    //    world.add(make_shared<triangle>(t));
+    //}
 
     // Camera
 
     camera cam;
 
-    cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width  = 400;
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
 
+    cam.vfov     = 20;
+    cam.lookfrom = point3(-2,2,1);
+    cam.lookat   = point3(0,0,-1);
+    cam.vup      = vec3(0,1,0);
+    
     Magick::Image image = cam.render(world);
-    image.write("./assets/images/seahorse-diffuse.png");
+    image.write("./assets/images/teste.png");
 
     return 0;
 }
