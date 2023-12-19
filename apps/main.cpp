9f9c8954f0635897c6fd33a92ea2fe1ec6ab3318
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2023-12-19 13:58:48 -0300

    fuzzed metal

--- apps/main.cpp
+++ apps/main.cpp
@@ -33,8 +33,8 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
@@ -66,7 +66,7 @@ int main() {
     cam2.vup      = vec3(0,1,0);*/
     
     Magick::Image image = cam.render(world);
-    image.write("./assets/images/shiny-metal.png");
+    image.write("./assets/images/fuzzed-metal.png");
 
     /*Magick::Image image2 = cam2.render(world);
     image2.write("./assets/images/activity05-viewpoints2.png");*/
